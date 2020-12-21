4bc1b691db506b4e6235f1f79ee50c47c3029b3e
MACBOOK <macbook@MACBOOKs-MacBook-Pro-2.local>
2020-12-21 17:44:13 +0700

    add file

--- Library/Mobile Documents/com~apple~CloudDocs/Nodejs/blog/src/index.js
+++ Library/Mobile Documents/com~apple~CloudDocs/Nodejs/blog/src/index.js
@@ -4,9 +4,18 @@ const path = require('path');
 var morgan = require('morgan');
 const handlebars  = require('express-handlebars');
 const { dirname } = require('path');
+const { link } = require('fs');
+
+
 const port = 3000;
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded({
+  extendeds: true
+}));
+
+app.use(express.json());
 //HTTP logger
+//
 app.use(morgan('combined'));
 
 // template engine
@@ -16,10 +25,24 @@ app.engine('hbs', handlebars({
 app.set('view engine', 'hbs');// 
 app.set('views', path.join(__dirname, 'resources/views'));
 console.log('PATH: ',path.join(__dirname, 'resources/views'));
+
+
+// routes init
+
+
 app.get('/Home', (req, res) => {
   res.render('home');
+})
+
+app.get('/news', (req, res) => {
+  res.render('news');
+})
+
+app.get('/search', (req, res) => {
+  res.render('search');
 })
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 })
+
